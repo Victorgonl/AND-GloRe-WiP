52fbe6c3c2da796c3c15b8872bb5ff613a040326
victorgonl <victorgonl@outlook.com>
2026-08-12 22:31:06 +0000

    New parameter to generate networks

--- scripts/generate_networks.py
+++ scripts/generate_networks.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from andglore.networks import build_ambiguous_networks
-
 from andglore.utils import load_yaml_config, parse_args
 
 
@@ -9,6 +8,8 @@ def generate_networks(
     dataset_name: str,
     selected_names: Optional[list[str]],
     splits: list[str],
+    max_orgs_per_author: Optional[int] = None,
+    max_author_paper_ratio: Optional[float] = None,
 ):
     """
     Generate AND Networks using preprocessed.csv and features.pt.
@@ -22,22 +23,15 @@ def generate_networks(
         embeddings_path=f"data/{dataset_name}/features.pt",
         splits=splits,
         selected_names=selected_names,
+        max_orgs_per_author=max_orgs_per_author,
+        max_author_paper_ratio=max_author_paper_ratio,
     )
 
 
 def main():
     args = parse_args()
     config = load_yaml_config(args.config)
-
-    dataset_name = config["dataset_name"]
-    selected_names = config["selected_names"]
-    splits = config["splits"]
-
-    generate_networks(
-        dataset_name=dataset_name,
-        selected_names=selected_names,
-        splits=splits,
-    )
+    generate_networks(**config)
 
 
 if __name__ == "__main__":
